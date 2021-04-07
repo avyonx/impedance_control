d2e6d6006db608201002ef8f9b97f2af7a6d0103
aivano <antun.ivanovic@fer.hr>
2021-04-07 14:41:57 +0200

    Change initial takeoff to trajectory reference.

--- scripts/get_uav_airborne.sh
+++ scripts/get_uav_airborne.sh
@@ -8,21 +8,32 @@ echo "Calling service to start manipulator control."
 rosservice call /$namespace/aerial_manipulator_control/start "data: true"
 sleep 2
 echo "Publishing takeoff pose."
-rostopic pub -1 /$namespace/aerial_manipulator_control/pose_stamped_ref_input geometry_msgs/PoseStamped "header:
-  seq: 0
-  stamp:
-    secs: 0
-    nsecs: 0
-  frame_id: ''
-pose:
-  position:
-    x: 2.0
-    y: 2.0
-    z: 2.0
-  orientation:
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0"
+#rostopic pub -1 /$namespace/aerial_manipulator_control/pose_stamped_ref_input geometry_msgs/PoseStamped "header:
+#  seq: 0
+#  stamp:
+#    secs: 0
+#    nsecs: 0
+#  frame_id: ''
+#pose:
+#  position:
+#    x: 2.0
+#    y: 2.0
+#    z: 2.0
+#  orientation:
+#    x: 0.0
+#    y: 0.0
+#    z: 0.0
+#    w: 1.0"
+rostopic pub -1 /$namespace/aerial_manipulator_control/trajectory_ref_input trajectory_msgs/JointTrajectoryPoint "positions: [1.37,2.2,2.0,0,0,0,0,0,0,0,0,0,0,0,0,1]                            
+velocities: [0]
+accelerations: [0]
+effort: [0]
+time_from_start: {secs: 0, nsecs: 0}"
+# Wait to get airborne and then zero forces
+sleep 8
+echo "Calling service to zero force sensor measurements."
+rosservice call /$namespace/force_filter/zero_all "{}"
+
 #rosservice call /$namespace/plan_wall_path "data: True"
 #rosservice call /$namespace/execute_wall_path "{}"
+
